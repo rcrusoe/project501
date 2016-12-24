9ead901a0ed1707180fd4e59818dae551410845e
Robinson Greig <robinsongreig@gmail.com>
2016-12-24 09:29:53 -0500

    add devise user

---
+++ db/migrate/20161224140410_add_details_to_user.rb
@@ -0,0 +1,16 @@
+class AddDetailsToUser < ActiveRecord::Migration
+  def change
+    add_column :users, :first_name, :string
+    add_column :users, :last_name, :string
+    add_column :users, :tagline, :string
+    add_column :users, :calendly_url, :string
+    add_column :users, :catalant_url, :string
+    add_column :users, :linkedin_url, :string
+    add_column :users, :twitter_url, :string
+    add_column :users, :medium_url, :string
+    add_column :users, :github_url, :string
+    add_column :users, :dribbble_url, :string
+    add_column :users, :website_url, :string
+    add_column :users, :approved, :binary
+  end
+end
