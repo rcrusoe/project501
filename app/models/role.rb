be6246906cafa3084bb0ef2bbdc5be6aa6cea921
Robinson Greig <robinsongreig@gmail.com>
2016-12-28 10:47:18 -0500

    clean up

--- app/models/role.rb
+++ app/models/role.rb
@@ -2,4 +2,7 @@ class Role < ActiveRecord::Base
     belongs_to :project
     belongs_to :user
 
+    scope :owner, -> { where('status = ?', "owner") }
+    scope :applicant, -> { where('status = ?', "applicant") }
+    scope :volunteer, -> { where('status = ?', "volunteer") }
 end
