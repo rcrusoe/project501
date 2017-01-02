d0add8b742b30a9625fe349d62dbc805109894c8
Robinson Greig <robinsongreig@gmail.com>
2017-01-01 20:10:19 -0500

    sanitize urls and clean up user profile block

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -1,7 +1,14 @@
 class Organization < ApplicationRecord
+  before_save :sanitize_urls
 
   has_many :memberships
   has_many :users, through: :memberships
 
   scope :approved, -> {where("approved = ?", true)}
+
+  def sanitize_urls
+    unless  self.website.empty? || self.website.include?('http://') || self.website.include?('https://')
+        self.website = 'http://' + self.website
+    end
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+  before_save :sanitize_urls
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
@@ -20,6 +22,40 @@ class User < ApplicationRecord
       first_name + ' ' + last_name
     else
       email.split('@')[0]
+    end
+  end
+
+  def sanitize_urls
+    unless  self.calendly_url.empty? || self.calendly_url.include?('http://') || self.calendly_url.include?('https://')
+      self.calendly_url = 'http://' + self.calendly_url
+    end
+
+    unless  self.catalant_url.empty? || self.catalant_url.include?('http://') || self.catalant_url.include?('https://')
+      self.catalant_url = 'http://' + self.catalant_url
+    end
+
+    unless  self.linkedin_url.empty? || self.linkedin_url.include?('http://') || self.linkedin_url.include?('https://')
+      self.linkedin_url = 'http://' + self.linkedin_url
+    end
+
+    unless  self.twitter_url.empty? || self.twitter_url.include?('http://') || self.twitter_url.include?('https://')
+      self.twitter_url = 'http://' + self.twitter_url
+    end
+
+    unless  self.medium_url.empty? || self.medium_url.include?('http://') || self.medium_url.include?('https://')
+      self.medium_url = 'http://' + self.medium_url
+    end
+
+    unless  self.github_url.empty? || self.github_url.include?('http://') || self.github_url.include?('https://')
+      self.github_url = 'http://' + self.github_url
+    end
+
+    unless  self.dribbble_url.empty? || self.dribbble_url.include?('http://') || self.dribbble_url.include?('https://')
+      self.dribbble_url = 'http://' + self.dribbble_url
+    end
+
+    unless  self.website_url.empty? || self.website_url.include?('http://') || self.website_url.include?('https://')
+      self.website_url = 'http://' + self.website_url
     end
   end
 end
