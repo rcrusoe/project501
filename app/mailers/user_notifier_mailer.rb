2b7399f9ae8a25ed939992e7698d4e51c1d21500
Robinson Greig <robinsongreig@gmail.com>
2017-01-02 10:20:53 -0500

    cleaned up edit user page

--- app/mailers/user_notifier_mailer.rb
+++ app/mailers/user_notifier_mailer.rb
@@ -1,5 +1,5 @@
 class UserNotifierMailer < ApplicationMailer
-  default :from => 'robinsongreig@gmail.com'
+  default from: 'robinsongreig@gmail.com'
 
   # send a signup email to the user, pass in the user object that   contains the user's email address
   def send_signup_email(author, receiver, conversation, topic, personal_message)
@@ -8,7 +8,8 @@ class UserNotifierMailer < ApplicationMailer
     @conversation = conversation
     @topic = topic
     @personal_message = personal_message
-    mail( :to => @receiver.email,
-    :subject => "New message 7 " + @author.name )
+    @from =  "Project 501 <" + @conversation.id.to_s + "@project501.com>"
+    mail( to: @receiver.email,
+    subject: "New message from " + @author.name, from: @from )
   end
 end
