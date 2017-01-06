76caf3a79c1ef952c5377ec5a54012f8d8969f3f
Robinson Greig <robinsongreig@gmail.com>
2017-01-06 08:06:10 -0500

    adjusted org form copy, added user create slack notification

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,8 +1,24 @@
 class RegistrationsController < Devise::RegistrationsController
 
-    protected
-
-    def after_update_path_for(resource)
-      user_path(resource)
+  def update
+    super do
+      if @user.save
+        user_info = {
+          pretext: "A user has updated their account.",
+          fallback: "#{@user.name}: #{@user.tagline}",
+          title: "#{@user.name}",
+          title_link: "#{project_url(@user)}",
+          text: "#{@user.tagline}",
+          color: "#BDD6DD",
+        }
+        PROJECT501_NOTIFIER.ping(attachments: [user_info])
+      end
     end
+  end
+
+  protected
+
+  def after_update_path_for(resource)
+    user_path(resource)
+  end
 end
