6e96c90c2c3cfb8d85d63ba1d5f36f1947d95fbd
Robinson Greig <robinsongreig@Robinsons-MacBook-Pro.local>
2017-01-25 14:54:10 -0800

    rolling back to undo that last change

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,7 +84,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = {host: "www.project501.com"}
+  config.action_mailer.default_url_options = {host: "project501.com"}
 
   # configure app to use SSL
   config.force_ssl = true
